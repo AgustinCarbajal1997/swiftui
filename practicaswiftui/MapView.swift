4ea63a176d177cc85dfd8e5c61da2967bff334a5
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-09 21:36:36 -0300

    [feature] insert UIKit components into SwiftUI Views

--- practicaswiftui/MapView.swift
+++ practicaswiftui/MapView.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 40.13, longitude: -8.20)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        uiView.setRegion(region, animated: true)
     }
 }
 
