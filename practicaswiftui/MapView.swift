97e19db0e44bfe66c4d15868280d8a5beb7ded7a
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-09 21:36:32 -0300

    [feature] insert UIKit components into SwiftUI Views

---
+++ practicaswiftui/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  practicaswiftui
+//
+//  Created by Agustin Carbajal on 09/11/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
