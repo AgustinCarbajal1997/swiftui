8e3feea486a4e167ad943594e77c27b79e884ec9
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-09 21:52:43 -0300

    [feature] insert UIKit components into SwiftUI Views

--- practicaswiftui/MainView.swift
+++ practicaswiftui/MainView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                MapView().frame(height: 300)
+                Divider().padding(10)
+                ImageView()
+                ContentView()
+            }
+
+        }
     }
 }
 
