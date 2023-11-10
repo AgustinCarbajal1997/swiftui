1013310829cb2bac3e1ab5f1cebc2ec3a48fdc09
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-10 17:22:40 -0300

    [feature] navigation between screens

---
+++ practicaswiftui/ListDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ListDetailView.swift
+//  practicaswiftui
+//
+//  Created by Agustin Carbajal on 10/11/2023.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListDetailView()
+    }
+}
