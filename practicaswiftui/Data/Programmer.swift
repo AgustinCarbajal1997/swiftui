af318b4072a2af703f5361118f72a82ba980b9e0
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-10 17:02:52 -0300

    [feature] List in swiftUI

---
+++ practicaswiftui/Data/Programmer.swift
@@ -0,0 +1,8 @@
+//
+//  Programmer.swift
+//  practicaswiftui
+//
+//  Created by Agustin Carbajal on 10/11/2023.
+//
+
+import Foundation
---
+++ practicaswiftui/RowView.swift
@@ -0,0 +1,20 @@
+//
+//  RowView.swift
+//  practicaswiftui
+//
+//  Created by Agustin Carbajal on 10/11/2023.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView()
+    }
+}
